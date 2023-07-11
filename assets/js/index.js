d7599d323e55aafa9ea5755ba6f9352922b91430
Brian Gorman <briangorman99@gmail.com>
2023-07-11 14:28:06 -0400

    change units label text when switch state changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-$(function () {});
+$(function () {
+  // Listen for units switch change
+  $("#units-switch").on("change", function () {
+    $(this)
+      .siblings("label")
+      .text($(this).is(":checked") ? "Fahrenheit" : "Celsius");
+  });
+});

b32a8d646469cdd8f534aa3e8c94e882303219c8
Brian Gorman <briangorman99@gmail.com>
2023-07-11 15:41:22 -0400

    refactor createUrl, add createGeoUrl and createForecastUrl functions, add function descriptions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
-let base = "api.openweathermap.org/data/2.5/forecast?";
-
 let apiKey = "";
 let baseUrl = "http://api.openweathermap.org/";
 
+/**
+ * Creates an OWM API URL
+ * @param {string} resource Location of the API resource
+ * @param  {...string} options Additional options
+ * @returns {string} Constructed OWM API URL
+ */
 function createUrl(resource, ...options) {
   let url = baseUrl + resource;
   if (options.length > 0) {
@@ -14,6 +18,28 @@ function createUrl(resource, ...options) {
   }
   url += "&appid=" + apiKey;
   return url;
+}
+
+/**
+ * Creates an OWM API URL for the Geocoding service based on city name
+ * @param {string} city Name of the city
+ * @param {number} limit Number of results to return
+ * @param  {...string} options Additional options
+ * @returns Constructed Geocoding API URL
+ */
+function createGeoUrl(city, limit = 1, ...options) {
+  return createUrl("geo/1.0/direct", `q=${city}`, `limit=${limit}`, ...options);
+}
+
+/**
+ * Creates an OWM API URL for the 5-Day Forecast based on geodetic coordinates
+ * @param {number} lat Latitudinal coordinate
+ * @param {number} lon Longitudinal coordinate
+ * @param  {...any} options Additional options
+ * @returns Constructed 5-Day Forecast API URL
+ */
+function createForecastUrl(lat, lon, ...options) {
+  return createUrl("data/2.5/forecast", lat, lon, ...options);
 }
 
 $(function () {
@@ -29,6 +55,6 @@ $(function () {
 
     let cityName = $("#city-name").val();
 
-    let geoUrl = createUrl("geo/1.0/direct", `q=${cityName}`, "limit=1");
+    let geoUrl = createGeoUrl(cityName, 1);
   });
 });
